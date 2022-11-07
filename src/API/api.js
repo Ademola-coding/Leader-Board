3d73546b690f8cda1d89f99cc199667cc946b1b5
ademola <ademolaowoeye07@gmail.com>
2022-11-07 19:44:23 +0100

    rearranged syntax

--- src/API/api.js
+++ src/API/api.js
@@ -1,35 +1,36 @@
 class LeaderBoard {
-  constructor() {
-    this.url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
-  }
+    constructor() {
+        this.url =
+            'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+    }
 
-  async Game(gameName) {
-    const response = await fetch(this.url, {
-      method: 'POST',
-      body: JSON.stringify({ name: gameName }),
-      headers: { 'Content-type': 'application/json; charset=UTF-8' },
-    });
+    async Game(gameName) {
+        const response = await fetch(this.url, {
+            method: 'POST',
+            body: JSON.stringify({ name: gameName }),
+            headers: { 'Content-type': 'application/json; charset=UTF-8' },
+        });
 
-    const recievedResponse = await response.json();
-    return recievedResponse;
-  }
+        const recievedResponse = await response.json();
+        return recievedResponse;
+    }
 
-  async getScores(gameID) {
-    const response = await fetch(`${this.url}${gameID}/scores/`);
-    const recievedResponse = await response.json();
-    return recievedResponse;
-  }
+    async getScores(gameID) {
+        const response = await fetch(`${this.url}${gameID}/scores/`);
+        const recievedResponse = await response.json();
+        return recievedResponse;
+    }
 
-  async postScore(gameID, username, score) {
-    const response = await fetch(`${this.url}${gameID}/scores/`, {
-      method: 'POST',
-      body: JSON.stringify({ user: username, score }),
-      headers: { 'Content-type': 'application/json; charset=UTF-8' },
-    });
+    async postScore(gameID, username, score) {
+        const response = await fetch(`${this.url}${gameID}/scores/`, {
+            method: 'POST',
+            body: JSON.stringify({ user: username, score }),
+            headers: { 'Content-type': 'application/json; charset=UTF-8' },
+        });
 
-    const recievedResponse = await response.json();
-    return recievedResponse;
-  }
+        const recievedResponse = await response.json();
+        return recievedResponse;
+    }
 }
 
 export default LeaderBoard;
--- src/index.js
+++ src/index.js
@@ -12,27 +12,27 @@ const LBA = new LeaderBoard();
 
 let gameId;
 document.addEventListener('DOMContentLoaded', () => {
-  LBA.Game('my cool new game')
-    .then((response) => response.result.split(' '))
-    .then((res) => {
-      [gameId] = [res[3]];
-    });
+    LBA.Game('my cool new game')
+        .then((response) => response.result.split(' '))
+        .then((res) => {
+            [gameId] = [res[3]];
+        });
 });
 
 Form.addEventListener('submit', (e) => {
-  LBA.postScore(gameId, player.value, playerScore.value);
-  player.value = '';
-  playerScore.value = '';
-  e.preventDefault();
+    LBA.postScore(gameId, player.value, playerScore.value);
+    player.value = '';
+    playerScore.value = '';
+    e.preventDefault();
 });
 
 const returnedData = (Data) => {
-  list.innerHTML = '';
-  Data.forEach((item) => {
-    list.innerHTML += `<li class="item">${item.user} : ${item.score}</li>`;
-  });
+    list.innerHTML = '';
+    Data.forEach((item) => {
+        list.innerHTML += `<li class="item">${item.user} : ${item.score}</li>`;
+    });
 };
 
 refreshBtn.addEventListener('click', () => {
-  LBA.getScores(gameId).then((resData) => returnedData(resData.result));
+    LBA.getScores(gameId).then((resData) => returnedData(resData.result));
 });
